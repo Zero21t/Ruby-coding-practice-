54c5c3ea0bd10904142f54154d32416f7720d6c4
Aman Toor <123129010+Zero21t@users.noreply.github.com>
2023-03-03 18:54:26 +0000

    Pending changes exported from your codespace

---
+++ main.rb
@@ -0,0 +1,14 @@
+#print commands
+puts 3+3    #calculations
+puts "hello world"  #texts
+
+#methods
+def hi 
+    puts "methods"
+end
+hi
+#or
+def names(name)
+    puts "hello #{name}"
+end
+names("Zero")
